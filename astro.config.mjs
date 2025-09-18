ce48cdd74887a74779ef0e6de3141c069923d3dd
Mu-Te Joshua Lau <71618875+JoshuaLau0220@users.noreply.github.com>
2025-09-18 16:22:56 -0500

    cicd: deploy to github.io

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://joshmtlau.github.io',
+    // base: '/repo-name' // not needed for this repo
+});
