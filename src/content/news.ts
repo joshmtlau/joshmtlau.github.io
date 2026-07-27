a4d4433f6b556a06d9ab4b3ba457ad72d68218b7
Mu-Te Joshua Lau <71618875+JoshuaLau0220@users.noreply.github.com>
2026-07-27 17:17:11 -0500

    add: new news item for SEQC paper acceptance at QCE'26

--- src/content/news.ts
+++ src/content/news.ts
@@ -2,13 +2,18 @@ import { formatDate } from "../utils/date";
 
 const newsItems = [
     {
+      date: new Date(Date.parse("2026-07-07")),
+      content: `Our paper SEQC: Stratify-Elaborate Quantum Compilation Towards Modular Hybrid Architectures ` +
+               `got accepted into <a href="https://qce.quantum.ieee.org/2026/" class="link">QCE'26</a>! See you in Toronto.`,
+    },
+    {
       date: new Date(Date.parse("2025-09-16")),
       content: "I began my PhD study at CS, Northwestern University!",
     },
     {
       date: new Date(Date.parse("2025-07-08")),
       content: `Our paper <a href="https://arxiv.org/abs/2508.04092" class="link">A Lazy Resynthesis Approach for Simultaneous T Gate and Two-Qubit Gate Optimization of Quantum Circuits</a> ` +
-               `got accepted into <a href="https://qce.quantum.ieee.org/2025/" class="link">QCE'25</a>! `,
+               `got accepted into <a href="https://qce.quantum.ieee.org/2025/" class="link">QCE'25</a>! See you in Albuquerque.`,
     },
     {
       date: new Date(Date.parse("2025-11-29")),
